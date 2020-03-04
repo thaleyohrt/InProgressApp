30b23186eecc5f63f01e9a7a1ffaf8f2740c05e9
Sean Clayton <sean.clayto@gmail.com>
2020-03-04 14:20:17 -0800

    setting up broken login

---
+++ script/firebase_api_ip.js
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------
+// Your web app's Firebase configuration (9 lines of code)
+// Replace the configuration with YOUR project's API information
+// copied from the firebase console (settings) of your project.
+//---------------------------------------------------------------------
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBtPVBxeOUiMGOb0JkfcmrjAo-cBny5ysI",
+    authDomain: "inprogress-7d731.firebaseapp.com",
+    databaseURL: "https://inprogress-7d731.firebaseio.com",
+    projectId: "inprogress-7d731",
+    storageBucket: "inprogress-7d731.appspot.com",
+    messagingSenderId: "840287480599",
+    appId: "1:840287480599:web:51544848c551cc39c1a552"
+};
